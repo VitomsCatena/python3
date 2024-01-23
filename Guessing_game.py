304fa5151b635b78e42ed620b4936d89bba6b829
VitomsCatena <105198927+VitomsCatena@users.noreply.github.com>
2024-01-23 16:56:38 +0300

    guessing game

--- Guessing_game.py
+++ Guessing_game.py
@@ -2,7 +2,7 @@ secret_number = 4
 guess_count = 0
 guess_limit = 3
 while guess_count < guess_limit:
-guess = int(input("Guess: "))
+  guess = int(input("Guess: "))
     guess_count += 1
     if guess == secret_number:
         print("you are awesome!")
