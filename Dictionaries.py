17e8134828660519826488b90b90b38e4a1bedcc
VitomsCatena <105198927+VitomsCatena@users.noreply.github.com>
2024-02-12 10:18:10 +0300

    dictionary

--- Dictionaries.py
+++ Dictionaries.py
@@ -1,10 +1,9 @@
 # currley {} are used to define a dictionary
 
 client {
-
-"name":"teddy smith",
-"age": 23,
-"is sad": true;
+  "name":"teddy smith",
+   "age": 23,
+   "is sad": true;
 }
 
 print(client["name"])
